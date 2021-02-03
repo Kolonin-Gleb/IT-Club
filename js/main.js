ae476a9ff4513016ce25e98b1a6efe7de5568550
tea-wizard <78158852+tea-wizard@users.noreply.github.com>
2021-02-03 18:38:40 +0300

    сложна
    js (я пыталась что-то сделать)

--- js/main.js
+++ js/main.js
@@ -6,8 +6,13 @@ eventsList.push(
     new EventsSideBlock("слева горы", "", "", sideBlock),
     new EventsSideBlock("а вдали", "", "", sideBlock),
     new EventsSideBlock("КАВКАЗ", "", "", sideBlock),
-    new EventsSideBlock("крутотенюшка", "", "", sideBlock)
+    new EventsSideBlock("крутотенюшка", "red", "", sideBlock)
 
 );
 
 mainBlock.innerHTML = eventsList[eventsList.length - 1].title;
+
+mainBlock.style.backgroundColor = eventsList[eventsList.length - 1].background; 
+// фон правого блока такой же, как у первого блока слева
+
+// mainBlock.style.backgroundColor = "red";
