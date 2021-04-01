82b68f18881c93917035441a3fcfc7f4143866f9
Колонин Глеб <topsecretway@yandex.ru>
2021-04-01 19:08:50 +0300

    Галерея заполнена
    Все планируемые к размещению события клуба добавлены в галерею.

--- js/main.js
+++ js/main.js
@@ -57,18 +57,47 @@ eventsList.push(
     // 2 Semestr
 
     new EventsSideBlock({
-        title: "sugar daddy", 
-        background: "url(https://birdinflight.imgix.net/wp-content/uploads/2019/02/moose-2.png?auto=format&q=70&fit=crop&crop=faces)", 
-        description: "oh my", 
+        title: "Создание карты сайта. Совет Молодоря Никиты", 
+        background: "url(images/gallery_of_events/2_Semestr/1_Nikita_site_map.jpg)", 
+        description: "Январь 2020 года. <br> Молодоря Никита помог с созданием карты сайта для нашего совместного проекта - сайта IT Club. <br> Все изменения, вносимые на сайт, что вы сейчас смотрите сначала тщательно продумываются на этой карте.", 
         daddy: sideBlock
     }),
 
     new EventsSideBlock({
-        title: "АДАДАДАДАДАДАДДАДА", 
-        background: "url(https://teknoblog.ru/wp-content/uploads/2020/08/Ofis.jpg)", 
-        description: "хахад какой-то", 
+        title: "Выступление Игоря Молчанова о SSH", 
+        background: "url(images/gallery_of_events/2_Semestr/2_Igor_SSH.jpg)", 
+        description: "Январь 2020 года. <br> Игорь Молчанов рассказал нам о сетевом протоколе SSH.", 
         daddy: sideBlock
-    })
+    }),
+
+    new EventsSideBlock({
+        title: "Мастер-класс Ефима Ланцмана по работе с Unity",
+        background: "url(images/gallery_of_events/2_Semestr/3_Efim_Unity.jpg)", 
+        description: "Февраль 2021 года. <br> Ефим Ланцман - студент-разработчик 2 курса провел мастер-класс по Unity. <br> По итогам мастер-класса мы написали код на C#, узнали что такое игровые движки и поработали в Unity.", 
+        daddy: sideBlock
+    }),
+
+    new EventsSideBlock({
+        title: "Мастер-класс Степанова Александра по работе с WordPress и OpenServer",
+        background: "url(images/gallery_of_events/2_Semestr/4_Sasha_WordPress.jpg)", 
+        description: "Февраль 2021 года. <br> Степанов Александр - программист 1 курса провел мастер-класс по работе с WordPress и OpenServer. <br> Мастер-класс был очень интересным и полезным. <br> Степанов Алексндр помог нам решить все появившиеся сложности и ответил на все наши вопросы.", 
+        daddy: sideBlock
+    }),
+
+    new EventsSideBlock({
+        title: "Мастер-класс Молодори Никиты по Swift",
+        background: "url(images/gallery_of_events/2_Semestr/5_Nikita_Swift.jpg)", 
+        description: "Март 2021 года. <br> Молодоря Никита - провел мастер-класс по программированию на Swift. <br> В течении мастер-класса велась разработка мобильного приложения, <br> которое следило за котировками валют.", 
+        daddy: sideBlock
+    }),
+
+    new EventsSideBlock({
+        title: "Мастер-класс Ефима Ланцмана по работе с Blender",
+        background: "url(images/gallery_of_events/2_Semestr/6_Efim_Blender.jpg)", 
+        description: "Март 2021 года. <br> Ефим Ланцман провел мастер-класс по 3D моделированию в Blender. <br> В результате мастер-класса были созданы модели средневековых мечей и освоены основы интерфеййса программы Blender 3D. <br> Ефим поделился материалами для дальнейшего изучения 3D моделирования.", 
+        daddy: sideBlock
+    }),
+    
 );
 
 Oliver(eventsList.length - 1);
