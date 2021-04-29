0bf52ccdc24a09bc809837a6cba6bf7d3bbb085f
tea-wizard <78158852+tea-wizard@users.noreply.github.com>
2021-04-29 20:04:12 +0300

    кнопки вне прокрутки
    кнопки для скролла находятся вне прокрутки

--- js/members/membersController.js
+++ js/members/membersController.js
@@ -4,20 +4,20 @@ let membersList = [];
 
 membersList.push(
     new Member({
-        name: "Дэнчик", 
+        name: "Хохлова Зоя Анатольевна", 
         age: "16", 
-        faculty: "Программист", 
-        photo: "",
+        faculty: "Разработчица", 
+        photo: "https://aif-s3.aif.ru/images/013/568/674f8c2ce9d3b1701fd367d54915598a.jpg",
         abstract: "рассказывает анекдоты на высшем уровне",
         cvLink: "*ссылка*",
         daddy: memberBlock
     }),
     
     new Member({
-        name: "Дэнчик", 
-        age: "16", 
-        faculty: "Программист", 
-        photo: "",
+        name: "Виноградова Юлианна Анастасьевна", 
+        age: "17", 
+        faculty: "Айтишница", 
+        photo: "https://st2.depositphotos.com/1024381/5201/i/600/depositphotos_52013547-stock-photo-mid-adult-woman.jpg",
         abstract: "рассказывает анекдоты на высшем уровне",
         cvLink: "*ссылка*",
         daddy: memberBlock
@@ -27,7 +27,7 @@ membersList.push(
         name: "Шахиджанян Владимир Владимирович", 
         age: "16", 
         faculty: "Программист", 
-        photo: "",
+        photo: "https://wl-adme.cf.tsp.li/resize/728x/jpg/72a/ea0/057bce523498aa17e4f3a9d074.jpg",
         abstract: "рассказывает анекдоты на высшем уровне рассказывает анекдоты на высшем уровне рассказывает",
         cvLink: "*ссылка*",
         daddy: memberBlock
