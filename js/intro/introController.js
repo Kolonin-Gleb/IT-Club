16352c0b6a1922b0170c44d547dee139c7536d60
tea-wizard <78158852+tea-wizard@users.noreply.github.com>
2021-05-27 19:38:10 +0300

    прокрутка с портретами
    реализовано внедрение картинок в прокрутку и бла-бла-бла
    мы чуть не умерли

---
+++ js/intro/introController.js
@@ -0,0 +1,7 @@
+let introPhotos = document.getElementById("intro");
+
+for (let i = 1; i <= 10; i++) {
+    let div = Object.assign(document.createElement("div"), {style: "width: 70px; height: 300px;"});
+    introPhotos.appendChild(div);
+    introPhotos.lastElementChild.style.background = `url(images/gallery_intro/${i}.jpg)`;
+}
