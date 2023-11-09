149d8e8584be913edb7189fa1039263f784e233a
Gleb Kolonin <topsecretway@yandex.ru>
2023-11-09 09:59:07 +0300

    Обновлён .gitignore
    Обновлён .gitignore и исключены лишние файлы

--- js/intro/introController.js
+++
@@ -1,7 +0,0 @@
-// let introPhotos = document.getElementById("intro");
-
-// for (let i = 1; i <= 10; i++) {
-//     let div = Object.assign(document.createElement("div"), {style: "width: 100px; height: 300px;"});
-//     introPhotos.appendChild(div);
-//     introPhotos.lastElementChild.style.background = `url(images/gallery_intro/${i}.jpg)`;
-// }
